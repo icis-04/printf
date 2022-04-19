04ce126bbc014ed9d7640e31cbd7770985902738
kingsmandralph <ekeneumegakwe@gmail.com>
2022-04-19 04:37:25 -0700

    .

--- main.h
+++ main.h
@@ -1,32 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdarg.h>
+#include <stdlib.h>
 
-int _printf(const char *format, ...)
+/**
+  *struct directives - holds fromat specifiers and their functions.
+  *@spec: char.
+  *@fspec: function pointer.
+  *
+  */
+typedef struct directives
 {
-int size = 0;
-char *p = NULL;
-va_list ap;
-va_start(ap, format);
-size = vsnprinf(p, size, format, ap);
-va_end(ap);
-if (size < 0)
-{
-return NULL;
-}
-size++;
-p = malloc(size);
-if (p == NULL)
-{
-return NULL;
-}
-va_start(ap, format);
-size = vsnprintf(p, size, format, ap);
-va_end(ap);
-if (size < 0)
-{
-free(p);
-return NULL;
-}
-};
+	char spec;
+	int (*fspec)(va_list, int);
+} forms;
 
+int _putchar(char c);
+int _printf(const char *format, ...);
+int (*get_print_func(char c))(va_list, int);
+int print_ch(va_list, int);
+int print_str(va_list, int);
+int print_int(va_list, int);
+int _putchar_int(int, int);
+int print_numbers(unsigned long n, unsigned int base, const char *digits);
+unsigned int find_length(unsigned int, int);
+int print_hex(va_list args, int len);
+int print_heX(va_list args, int len);
+int print_unsignd(va_list args, int len);
+int print_octal(va_list args, int len);
+int print_rot13(va_list args, int len);
+int print_b(va_list args, int len);
+int print_binary(unsigned int, int);
+int print_Str(va_list args, int len);
+
+#endif
