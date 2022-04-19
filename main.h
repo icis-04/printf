fa9939a94f23c91985ca63efaef323c6fefe5528
icis-04 <chidexiwuaku@gmail.com>
2022-04-19 04:29:21 -0700

    add

--- main.h
+++ main.h
@@ -1,33 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-char *
-int _printf(const char *format, ...)
-{
-int size = 0;
-char *p = NULL;
-va_list ap;
-va_start(ap, format);
-size = vsnprinf(p, size, format, ap);
-va_end(ap);
-if (size < 0)
-{
-return NULL;
-}
-size++;
-p = malloc(size);
-if (p == NULL)
-{
-return NULL;
-}
-va_start(ap, format);
-size = vsnprintf(p, size, format, ap);
-va_end(ap);
-if (size < 0)
-{
-free(p);
-return NULL;
-}
-return (p);
-}
+int _printf(const char *format, ...);
 
